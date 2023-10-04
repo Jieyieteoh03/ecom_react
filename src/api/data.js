4ced7b7d90b92da798054ad2408c686523f1911a
Teoh Jie Yie <jieyiet2003@gmail.com>
2023-10-04 11:32:21 +0800

    update

--- src/api/data.js
+++ src/api/data.js
@@ -1,4 +1,6 @@
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5000"
-    : "http://10.1.104.1:5000";
+// export const API_URL =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:5000"
+//     : "http://10.1.104.1:5000";
+
+export const API_URL = "http://localhost:5000/";
